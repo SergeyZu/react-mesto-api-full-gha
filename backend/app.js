4cacbc215616b86332d5db1513b580e9752648b4
SergeyZu <vechniystudent19@yandex.ru>
2023-06-27 12:21:03 +0700

    test

--- backend/app.js
+++ backend/app.js
@@ -44,11 +44,11 @@ app.use(limiter);
 
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
 
 app.use(router);
 
